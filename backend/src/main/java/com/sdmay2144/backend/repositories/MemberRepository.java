d0740e6fe940f96ab61ed6cf988138a683203494
alexiscordts <alexis.cordts@gmail.com>
2020-09-23 11:55:06 -0500

    Backend running with test model/repo/controller/service

---
+++ backend/src/main/java/com/sdmay2144/backend/repositories/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.sdmay2144.backend.repositories;
+
+import com.sdmay2144.backend.models.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("memberRepository")
+public interface MemberRepository 
+       extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
+}
