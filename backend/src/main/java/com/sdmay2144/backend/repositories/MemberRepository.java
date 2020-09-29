7ae45af69825f3b3634fba71bbc47d88767ff6e7
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2020-09-29 16:39:05 -0500

    Formatting changes

--- backend/src/main/java/com/sdmay2144/backend/repositories/MemberRepository.java
+++ backend/src/main/java/com/sdmay2144/backend/repositories/MemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("memberRepository")
-public interface MemberRepository 
-       extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByEmail(String email);
 }
