ce9c706abebbb8a3b9a23b377cff76a870ac174d
Megan Bailey <megbailey16@gmail.com>
2020-11-03 10:30:02 -0600

    removed unused code; allowed inputs to be editable on the edit screen:

--- frontend/src/EditTherapist.js
+++ frontend/src/EditTherapist.js
@@ -5,26 +5,26 @@ import Nav from "./Nav";
 class EditTherapist extends React.Component {
 
     
-      render() {
-        return (
-          <div >
-            <Nav/>
-            <div class="formScreen">
-              <div class="form-style">
-                <div class="form-style-heading"> Edit Therapist Information</div>
-                  <form action="" method="post">
-                    <label for="fname"><span>First Name <span class="required">*</span></span><input type="text" class="input-field" name="fname" value={sessionStorage.getItem("fname")} /></label>
-                    <label for="lname"><span>Last Name <span class="required">*</span></span><input type="text" class="input-field" name="lname" value={sessionStorage.getItem("lname")} /></label>
-                    <label for="email"><span>E-mail <span class="required">*</span></span><input type="text" class="input-field" name="email" value={sessionStorage.getItem("email")} /></label>
-                    <div class="buttonContainer">
-                      <input type="button" onclick="alert('Hello World!')" value="Delete"/>
-                      <input type="submit" value="Save" />
-                    </div>
-                  </form>
+  render() {
+    return (
+      <div >
+        <Nav/>
+        <div class="formScreen">
+          <div class="form-style">
+            <div class="form-style-heading"> Edit Therapist Information</div>
+              <form action="" method="post">
+                <label for="fname"><span>First Name <span class="required">*</span></span><input type="text" class="input-field" name="fname" defaultValue={sessionStorage.getItem("fname")} /></label>
+                <label for="lname"><span>Last Name <span class="required">*</span></span><input type="text" class="input-field" name="lname" defaultValue={sessionStorage.getItem("lname")} /></label>
+                <label for="email"><span>E-mail <span class="required">*</span></span><input type="text" class="input-field" name="email" defaultValue={sessionStorage.getItem("email")} /></label>
+                <div class="buttonContainer">
+                  <input type="button" value="Delete"/>
+                  <input type="submit" value="Save" />
                 </div>
+              </form>
             </div>
           </div>
-        );
-      }
+        </div>
+      );
     }
+  }
 export default EditTherapist;
