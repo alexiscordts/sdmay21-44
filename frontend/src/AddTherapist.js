db8372d0d2144ead79b5c85fe1d3e4eaeb07a280
Megan Bailey <megbailey16@gmail.com>
2020-11-11 20:05:22 -0600

    Added UI for inputing therapy type when creating therapist user.

--- frontend/src/AddTherapist.js
+++ frontend/src/AddTherapist.js
@@ -16,6 +16,22 @@ class AddTherapist extends React.Component {
                     <label for="fname"><span>First Name <span class="required">*</span></span><input type="text" class="input-field" name="fname" value="" /></label>
                     <label for="lname"><span>Last Name <span class="required">*</span></span><input type="text" class="input-field" name="lname" value="" /></label>
                     <label for="email"><span>E-mail <span class="required">*</span></span><input type="text" class="input-field" name="email" value="" /></label>
+                    <label><span>Therapy Type <span class="required">*</span></span>
+                      <div className="checkBoxArea">
+                        <label for = "chkPT">
+                          <input type = "checkbox" id = "chkPT" value = "PT"/>
+                          Physical Therapy
+                        </label>
+                        <label for = "chkOT">
+                          <input type = "checkbox" id = "chkOT" value = "OT"/>
+                          Occupational Therapy
+                        </label>
+                        <label for = "chkST">
+                          <input type = "checkbox" id = "chkST" value = "ST"/>
+                          Speech Therapy
+                        </label>
+                      </div>
+                    </label>
                     <p class="submitLabel">By clicking the button, an email will be sent to the Therapist with their login information.</p>
                     <div class="submitLabel"><input type="submit" value="Create" /></div>
                   </form>
