5244065f3109f8ecb43d671d824e521d7623f110
Megan Bailey <megbailey16@gmail.com>
2021-02-10 18:27:10 -0600

    Respect user input on add user pages

--- frontend/src/UserPages/AddNurse.js
+++ frontend/src/UserPages/AddNurse.js
@@ -1,28 +1,65 @@
-import React from "react";
+import React, {useState} from "react";
 import "../FormStyles.css";
 import Nav from "../Nav";
 
-class AddNurse extends React.Component {
+const AddNurse = () => {
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+  const [email, setEmail] = useState('');
 
-    
-      render() {
-        return (
-          <div >
-            <Nav/>
-            <div class="formScreen">
-              <div class="form-style">
-                <div class="form-style-heading"> Add a Nurse </div>
-                  <form action="" method="post">
-                    <label for="fname"><span>First Name <span class="required">*</span></span><input type="text" class="input-field" name="fname" value="" /></label>
-                    <label for="lname"><span>Last Name <span class="required">*</span></span><input type="text" class="input-field" name="lname" value="" /></label>
-                    <label for="email"><span>E-mail <span class="required">*</span></span><input type="text" class="input-field" name="email" value="" /></label>
-                    <p class="submitLabel">By clicking the button, an email will be sent to the Nurse with their login information.</p>
-                    <div class="submitLabel"><input type="submit" value="Create" /></div>
-                  </form>
-                </div>
-            </div>
+  return (
+    <div >
+      <Nav/>
+      <div class="formScreen">
+        <div class="form-style">
+          <div class="form-style-heading"> Add a Nurse </div>
+            <form action="" method="post">
+            <label for="fname">
+                  <span>
+                    First Name
+                    <span class="required">*</span>
+                  </span>
+                  <input 
+                    type="text" 
+                    class="input-field" 
+                    onChange={(e)=> setFName(e.target.value)} 
+                    name="fname" 
+                    value={fName}
+                  />
+                </label>
+                <label for="lname">
+                  <span>
+                    Last Name
+                    <span class="required">*</span>
+                  </span>
+                  <input 
+                    type="text" 
+                    class="input-field" 
+                    onChange={(e)=> setLName(e.target.value)} 
+                    name="lname" 
+                    value={lName}
+                  />
+                </label>
+                <label for="email">
+                  <span>
+                    E-mail
+                    <span class="required">*</span>
+                  </span>
+                  <input 
+                    type="text" 
+                    class="input-field" 
+                    onChange={(e)=> setEmail(e.target.value)} 
+                    name="email" 
+                    value={email}
+                  />
+                </label>
+              <p class="submitLabel">By clicking the button, an email will be sent to the Nurse with their login information.</p>
+              <div class="submitLabel"><input type="submit" value="Create" /></div>
+            </form>
           </div>
-        );
-      }
-    }
+      </div>
+    </div>
+  );
+};
+
 export default AddNurse;
--- frontend/src/UserPages/AddTherapist.js
+++ frontend/src/UserPages/AddTherapist.js
@@ -1,44 +1,81 @@
-import React from "react";
+import React, {useState} from "react";
 import "../FormStyles.css";
 import Nav from "../Nav";
 
-class AddTherapist extends React.Component {
-
-    
-      render() {
-        return (
-          <div >
-            <Nav/>
-            <div class="formScreen">
-              <div class="form-style">
-                <div class="form-style-heading"> Add a Therapist </div>
-                  <form action="" method="post">
-                    <label for="fname"><span>First Name <span class="required">*</span></span><input type="text" class="input-field" name="fname" value="" /></label>
-                    <label for="lname"><span>Last Name <span class="required">*</span></span><input type="text" class="input-field" name="lname" value="" /></label>
-                    <label for="email"><span>E-mail <span class="required">*</span></span><input type="text" class="input-field" name="email" value="" /></label>
-                    <label><span>Therapy Type <span class="required">*</span></span>
-                      <div className="checkBoxArea">
-                        <label for = "chkPT">
-                          <input type = "checkbox" id = "chkPT" value = "PT"/>
-                          Physical Therapy
-                        </label>
-                        <label for = "chkOT">
-                          <input type = "checkbox" id = "chkOT" value = "OT"/>
-                          Occupational Therapy
-                        </label>
-                        <label for = "chkST">
-                          <input type = "checkbox" id = "chkST" value = "ST"/>
-                          Speech Therapy
-                        </label>
-                      </div>
+const AddTherapist = () => {
+    const [fName, setFName] = useState('');
+    const [lName, setLName] = useState('');
+    const [email, setEmail] = useState('');
+  
+    return (
+      <div >
+        <Nav/>
+        <div class="formScreen">
+          <div class="form-style">
+            <div class="form-style-heading"> Add a Therapist </div>
+              <form action="" method="post">
+              <label for="fname">
+                  <span>
+                    First Name
+                    <span class="required">*</span>
+                  </span>
+                  <input 
+                    type="text" 
+                    class="input-field" 
+                    onChange={(e)=> setFName(e.target.value)} 
+                    name="fname" 
+                    value={fName}
+                  />
+                </label>
+                <label for="lname">
+                  <span>
+                    Last Name
+                    <span class="required">*</span>
+                  </span>
+                  <input 
+                    type="text" 
+                    class="input-field" 
+                    onChange={(e)=> setLName(e.target.value)} 
+                    name="lname" 
+                    value={lName}
+                  />
+                </label>
+                <label for="email">
+                  <span>
+                    E-mail
+                    <span class="required">*</span>
+                  </span>
+                  <input 
+                    type="text" 
+                    class="input-field" 
+                    onChange={(e)=> setEmail(e.target.value)} 
+                    name="email" 
+                    value={email}
+                  />
+                </label>
+                <label><span>Therapy Type <span class="required">*</span></span>
+                  <div className="checkBoxArea">
+                    <label for = "chkPT">
+                      <input type = "checkbox" id = "chkPT" value = "PT"/>
+                      Physical Therapy
                     </label>
-                    <p class="submitLabel">By clicking the button, an email will be sent to the Therapist with their login information.</p>
-                    <div class="submitLabel"><input type="submit" value="Create" /></div>
-                  </form>
-                </div>
+                    <label for = "chkOT">
+                      <input type = "checkbox" id = "chkOT" value = "OT"/>
+                      Occupational Therapy
+                    </label>
+                    <label for = "chkST">
+                      <input type = "checkbox" id = "chkST" value = "ST"/>
+                      Speech Therapy
+                    </label>
+                  </div>
+                </label>
+                <p class="submitLabel">By clicking the button, an email will be sent to the Therapist with their login information.</p>
+                <div class="submitLabel"><input type="submit" value="Create" /></div>
+              </form>
             </div>
-          </div>
-        );
-      }
-    }
+        </div>
+      </div>
+    );
+  
+};
 export default AddTherapist;
