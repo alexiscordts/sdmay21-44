0f797fdf482a8c685a9da85b08919441c440f8cf
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2021-02-21 21:29:57 -0600

    Remove unnecssary options config in database api

--- C#Backend/InpatientTherapySchedulingProgram/Models/CoreDbContext.cs
+++ C#Backend/InpatientTherapySchedulingProgram/Models/CoreDbContext.cs
@@ -28,14 +28,14 @@ namespace InpatientTherapySchedulingProgram.Models
         public virtual DbSet<Therapy> Therapy { get; set; }
         public virtual DbSet<User> User { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=localhost;Database=inpatient_therapy;Trusted_Connection=True;");
             }
-        }
+        }*/
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
