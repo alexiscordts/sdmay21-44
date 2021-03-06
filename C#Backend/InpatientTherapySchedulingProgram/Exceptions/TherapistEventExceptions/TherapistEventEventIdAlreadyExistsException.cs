07064e66bc95e55aa722b7628e4b797ff100c79c
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2021-03-06 17:17:47 -0600

    Create therapist event service, create therapist event exceptions, update therapist event controller to use service

---
+++ C#Backend/InpatientTherapySchedulingProgram/Exceptions/TherapistEventExceptions/TherapistEventEventIdAlreadyExistsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InpatientTherapySchedulingProgram.Exceptions.TherapistEventExceptions
+{
+    [Serializable]
+    public class EventIdAlreadyExistsException : Exception
+    {
+        public EventIdAlreadyExistsException()
+        { }
+
+        public EventIdAlreadyExistsException(string message)
+            : base(message)
+        { }
+
+        public EventIdAlreadyExistsException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
