6e158baa403ada63ba5a5ee3ec503c1cf2c3c3a6
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2021-03-20 12:25:55 -0500

    Change app settings to allow for deployment on IIS

--- C#Backend/InpatientTherapySchedulingProgram/Startup.cs
+++ C#Backend/InpatientTherapySchedulingProgram/Startup.cs
@@ -34,7 +34,7 @@ namespace InpatientTherapySchedulingProgram
             //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             //    .AddMicrosoftIdentityWebApi(Configuration, "AzureAd");
 
-            services.AddDbContext<CoreDbContext>(op => op.UseSqlServer(Configuration.GetConnectionString("Database")));
+            services.AddDbContext<CoreDbContext>(op => op.UseSqlServer(Configuration.GetConnectionString("QA")));
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<ILocationService, LocationService>();
             services.AddScoped<ITherapyService, TherapyService>();
