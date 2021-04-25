86a2c45967938cfb47981c00c65eeee70e209082
Megan Bailey <megbailey16@gmail.com>
2021-04-25 13:49:59 -0500

    Initial setup for adding therapy types

--- frontend/src/TherapyTypes/AddTherapyTypes.js
+++ frontend/src/TherapyTypes/AddTherapyTypes.js
@@ -1,10 +1,42 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../FormStyles.css";
 import Nav from "../Nav";
+import axios from "axios";
 
-const AddTherapyTypes = () => {
-  const [name, setName] = useState('');
-  const [address, setSubtypes] = useState('');
+class AddTherapyTypes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      subtypes: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePost = this.handlePost.bind(this);
+  }
+
+  handlePost(event) {
+    event.preventDefault();
+    const url = "http://10.29.163.20:8081/api/therapy/"; 
+    axios
+      .post(url, this.state)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    setTimeout(function () {
+      window.location.href = "/view_therapy_types";
+    }, 2000);
+  }
+
+  handleChange(event) {
+    this.setState({
+    [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
 
   return (
     <div >
@@ -12,20 +44,21 @@ const AddTherapyTypes = () => {
       <div class="formScreen">
         <div class="form-style">
           <div class="form-style-heading"> Add a Therapy Type </div>
-            <form action="" method="post">
+            <form onSubmit={this.handlePost}>
               <label for="name">
                 <span>
                   Name
                   <span class="required">*</span>
                 </span>
-                <input type="text" class="input-field" name="name" value={name} onClick={(e)=>setName(e.target.value)}/>
+                <input type="text" class="input-field" name="name" onChange={this.handleChange}/>
               </label>
               <label for="name">
                 <span>
                   Subtypes
                   <span class="required">*</span>
                 </span>
-                <input type="text" class="input-field" name="subtype" value={address} onClick={(e)=>setSubtypes(e.target.value)}/>
+                <input type="text" class="input-field" name="subtypes" onChange={this.handleChange}/>
+                <p class="submitLabel">(Comma seperated values)</p>
               </label>
               <div class="submitLabel"><input type="submit" value="Create" /></div>
             </form>
@@ -33,5 +66,6 @@ const AddTherapyTypes = () => {
       </div>
     </div>
   );
+};
 };
 export default AddTherapyTypes;
