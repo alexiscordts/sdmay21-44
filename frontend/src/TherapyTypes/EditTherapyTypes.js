976ed54fc9d13f87ab466f8413b0669238560c58
Megan Bailey <megbailey16@gmail.com>
2021-04-27 15:43:42 -0500

    Finished edit

--- frontend/src/TherapyTypes/EditTherapyTypes.js
+++ frontend/src/TherapyTypes/EditTherapyTypes.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const EditTherapyTypes = () => {
   const subtypes = sessionStorage.getItem("subtypes").split(',');
+  const [newAdl, setNewAdl] = useState('');
 
   const deleteAdl = async (adl) => {
     const url = "http://10.29.163.20:8081/api/therapy/"+adl; 
@@ -19,6 +20,21 @@ const EditTherapyTypes = () => {
         console.log(error);
       });
       window.location.href = "/view_therapy_types";
+  }
+
+  const updateAdl = async(subtypeName, adl) => {
+    const url = "http://10.29.163.20:8081/api/therapy/"+subtypeName; 
+  
+     await axios
+      .delete(url)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      addAdl(adl);
   }
   
   const SubtypeInputs = ({subtypeName}) => {
@@ -38,27 +54,26 @@ const EditTherapyTypes = () => {
           setAdl(e.target.value)
         }}
       />
-      <input type="button" value="Delete" style={{marginLeft: "10px"}} onClick={() => deleteAdl(subtypeName)}/>
+      <input type="button" value="Delete" style={{marginLeft: "10px", marginRight: "5px"}} onClick={() => deleteAdl(subtypeName)}/>
       <input type="button" value="Save" onClick={()=> updateAdl(subtypeName, adl)}/>
     </div>
   )};
 
-  const updateAdl = async(subtypeName, adl) => {
-      const url = "http://10.29.163.20:8081/api/therapy/" + subtypeName; 
-  
-      await axios
-      .put(url, {
-        "adl": adl,
-        "therapyType": sessionStorage.getItem("name"),
-        "abbreviation": adl.split(/\s/).reduce((response,word)=> response+=word.slice(0,1),'')
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-   // window.location.href = "/view_therapy_types";
+  const addAdl = async(adl) => {
+    const url = "http://10.29.163.20:8081/api/therapy/"; 
+    await axios
+    .post(url, {
+      adl,
+      type: sessionStorage.getItem("name"),
+      abbreviation: adl.split(/\s/).reduce((response,word)=> response+=word.slice(0,1),'')
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  window.location.href = "/view_therapy_types";
   }
   
   return (
@@ -75,6 +90,22 @@ const EditTherapyTypes = () => {
                 </span>
                 <div className="subtypeInputContainer">
                   {subtypes.map((subtype) => <SubtypeInputs subtypeName={subtype} />)}
+                </div>
+                <div style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  marginBottom: "5px"}}>
+                  <input
+                    type="text"
+                    className="inputFieldSubtype"
+                    name={`subtypeNewAdl`}
+                    defaultValue={newAdl}
+                    style={{width: "225px", marginLeft: "100px", marginRight: "20px"}}
+                    onChange={(e)=> {
+                      setNewAdl(e.target.value)
+                    }}
+                  />
+                  <input type="button" value="Add" onClick={()=> addAdl(newAdl)}/>
                 </div>
               </label>
             </form>
